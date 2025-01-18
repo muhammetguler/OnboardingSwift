0c4874dc7ceca94fe1bc945b516437c8fa78f63c
Muhammet Güler <muhammet.guler@lcwaikiki.com>
2025-01-18 11:23:55 +0300

    Initial Commit

---
+++ OnboardingProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnboardingProject
+//
+//  Created by Muhammet Guler on 18.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
